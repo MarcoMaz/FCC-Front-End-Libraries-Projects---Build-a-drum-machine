3986f18b93ccb512e8d5555aee29958ab3d5e391
Marco Mazzai <mazzai.marco@gmail.com>
2019-08-04 15:04:49 +0200

    Updates

---
+++ precache-manifest.14fa338242b8452f08879ffc6a2cf0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007fdb18d910b7d6e852daa10c0027d4",
+    "url": "/drum-machine-react/index.html"
+  },
+  {
+    "revision": "0699f71a14d0ea9aecdd",
+    "url": "/drum-machine-react/static/css/main.622240ca.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/drum-machine-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "0699f71a14d0ea9aecdd",
+    "url": "/drum-machine-react/static/js/main.3d71a746.chunk.js"
+  },
+  {
+    "revision": "3333f7cc6246180a01cd",
+    "url": "/drum-machine-react/static/js/runtime~main.aca7fcdb.js"
+  }
+]);
